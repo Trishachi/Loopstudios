c2ed40b14ae05f88b7b84374feebc2f83c4a933c
Chisom Trisha Okoye <trisha.omabu@gmail.com>
2025-03-06 10:33:06 -0700

    Page header in Progress

---
+++ script.js
@@ -0,0 +1,9 @@
+function mobileMenu() {
+//     var x = document.getElementById("navbarButtons");
+//   if (x.className === "navbar__buttons") {
+//     x.className += " responsive";
+//   } else {
+//     x.className = "navbar__buttons";
+//   }
+alert("Button clicked");
+  }
