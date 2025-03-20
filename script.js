0928f452a5a7f5cebe5bb5cb37fdcc0966e5f2fe
Chisom Trisha Okoye <trisha.omabu@gmail.com>
2025-03-20 08:38:28 -0600

    Cleanup JS functions file

--- script.js
+++ script.js
@@ -1,12 +1,3 @@
-function mobileMenu() {
-  var x = document.getElementById("topnav");
-  if (x.className === "menu") {
-    x.className += " responsive";
-  } else {
-    x.className = "menu";
-  }
-}
-
 function showSidebar(){
   const sidebar = document.querySelector(".sidebar");
   sidebar.style.display = "flex";
